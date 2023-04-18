bcabc6859b33724e74ed51e4455361e36d6a6829
skvdmt <skvdmt@yandex.ru>
2023-04-18 15:31:02 +0300

    dev editor

--- data.go
+++ data.go
@@ -9,38 +9,36 @@ import (
 )
 
 // NewData returns a reference to a new type with data
-func NewData(connID int, table, row string, id int, datatype string, value interface{}) *Data {
+func NewData(connID int, sessionID, table, row string, id int, datatype string, value interface{}) *Data {
 	d := &Data{
-		ConnID:   connID,
-		table:    table,
-		row:      row,
-		id:       id,
-		DataType: datatype,
-		Value:    value,
-		Editable: true,
-		Key:      f.GetHash(),
+		ConnID:    connID,
+		table:     table,
+		row:       row,
+		id:        id,
+		DataType:  datatype,
+		Value:     value,
+		Editable:  true,
+		sessionID: sessionID,
 	}
 	return d
 }
 
 // Data data type for quick editing
 type Data struct {
-	ConnID   int
-	table    string
-	row      string
-	id       int
-	DataType string
-	Value    interface{}
-	Editable bool
-	Key      string
+	sessionID string
+	ConnID    int
+	table     string
+	row       string
+	id        int
+	DataType  string
+	Value     interface{}
+	Editable  bool
+	Key       string
 }
 
 // Edit preparation of a template that allows for quick editing on the page
 func (d *Data) Edit(db *sql.DB) string {
-	_, err := db.Query("insert into `editor` (`table`, `row_name`, `id_line`, `type`, `key`) VALUES (?, ?, ?, ?, ?)", d.table, d.row, d.id, d.DataType, d.Key)
-	if err != nil {
-		log.Println(err)
-	}
+	d.getKey(db)
 	var t bytes.Buffer
 	tfn := fmt.Sprintf("%s/templates/%s.gohtml", currentDir, d.DataType)
 	switch d.DataType {
@@ -50,4 +48,45 @@ func (d *Data) Edit(db *sql.DB) string {
 		t = TemplateAsHTML(tfn, d)
 	}
 	return t.String()
+}
+
+// getKey checks if the current user record key exists in the database and returns it.
+// Or creates and saves a new key and returns it
+func (d *Data) getKey(db *sql.DB) {
+	var sc int
+	err := db.QueryRow("select count(*) from `editor` where `session_id` = ? and `table` = ? and `row_name` = ? and `id_line` = ? and `type` = ?", d.sessionID, d.table, d.row, d.id, d.DataType).Scan(&sc)
+	if err != nil {
+		log.Println(err)
+	}
+	switch sc {
+	case 1:
+		// get key
+		err = db.QueryRow("select `key` from `editor` where `session_id` = ? and `table` = ? and `row_name` = ? and `id_line` = ? and `type` = ?", d.sessionID, d.table, d.row, d.id, d.DataType).Scan(&d.Key)
+		if err != nil {
+			log.Println(err)
+		}
+		// update last use
+		_, err = db.Exec("update `editor` set `dt_last_use` = CURRENT_TIMESTAMP where `key` = ?", d.Key)
+		if err != nil {
+			log.Println(err)
+		}
+	case 0:
+		// make new unique key
+		for {
+			d.Key = f.GetHash()
+			var kc int
+			err = db.QueryRow("select count(*) from `editor` where `key` = ?", d.Key).Scan(&kc)
+			if err != nil {
+				log.Println(err)
+			}
+			if kc == 0 {
+				break
+			}
+		}
+		// save key
+		_, err = db.Exec("insert into `editor` (`session_id`, `table`, `row_name`, `id_line`, `type`, `key`) VALUES (?, ?, ?, ?, ?, ?)", d.sessionID, d.table, d.row, d.id, d.DataType, d.Key)
+		if err != nil {
+			log.Println(err)
+		}
+	}
 }
--- editor.go
+++ editor.go
@@ -2,18 +2,25 @@
 package editor
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/skvdmt/f"
 	"log"
 	"net/http"
 	"os"
 	"path"
 	"runtime"
+	"time"
 )
 
 // currentDir is full path to package
 var currentDir string
 
+// DBC database connection settings
 var DBC Connections
+
+// editorSessionIDCookieName cookie name for editor session id
+var editorSessionIDCookieName = "EditorSessionID"
 
 // init get full path to package directory and make handler to api to save data
 func init() {
@@ -47,4 +54,41 @@ func GetCSS() string {
 		log.Println(err)
 	}
 	return string(data)
+}
+
+// GetSessionID return editor session id from cookies
+func GetSessionID(w http.ResponseWriter, r *http.Request, db *sql.DB) string {
+	for _, cookie := range r.Cookies() {
+		if cookie.Name == editorSessionIDCookieName {
+			return cookie.Value
+		}
+	}
+	return makeSession(w, db)
+}
+
+// makeSession creates a session and returns its id
+func makeSession(w http.ResponseWriter, db *sql.DB) string {
+	// creates a unique session ID
+	var sessionID string
+	for {
+		hash := f.GetHash()
+		var cnt int
+		err := db.QueryRow("select count(*) from `editor` where `session_id` = ?", hash).Scan(&cnt)
+		if err != nil {
+			log.Println(err)
+		}
+		if cnt == 0 {
+			sessionID = hash
+			break
+		}
+	}
+	// writes session ID in cookies
+	http.SetCookie(w, &http.Cookie{
+		Name:     editorSessionIDCookieName,
+		Value:    sessionID,
+		Path:     "/",
+		Expires:  time.Now().AddDate(1, 0, 0),
+		SameSite: http.SameSiteLaxMode,
+	})
+	return sessionID
 }
